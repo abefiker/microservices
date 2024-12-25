36f66a495f6de1a63c36b3f8f09dbed859b04b51
abfiker <new94abemelek@gmail.com>
2024-12-25 21:01:59 +0300

    am very happy to learn and practice unit test into every auth routes

---
+++ ticketing/auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { json } from 'body-parser';
+import { CurrentUserRouter } from './routes/current-user';
+import { SignupRouter } from './routes/signup';
+import { SigninRouter } from './routes/signin';
+import { SignoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(CurrentUserRouter);
+app.use(SignupRouter);
+app.use(SigninRouter);
+app.use(SignoutRouter);
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    errorHandler(err, req, res, next);
+  }
+);
+
+export { app };
--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,44 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import { json } from 'body-parser';
-import { CurrentUserRouter } from './routes/current-user';
-import { SignupRouter } from './routes/signup';
-import { SigninRouter } from './routes/signin';
-import { SignoutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(CurrentUserRouter);
-app.use(SignupRouter);
-app.use(SigninRouter);
-app.use(SignoutRouter);
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-app.use(
-  (
-    err: Error,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    errorHandler(err, req, res, next);
-  }
-);
+import { app } from './app';
 const start = async () => {
-  if(!process.env.JWT_KEY){
+  if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY environment variable is missing');
   }
   try {
